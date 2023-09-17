40002a46f99003d098b5c6dfad5e5465606ef2d0
MehMasha <mehmasha69@gmail.com>
2023-09-18 00:44:24 +0300

    docker compose fixes and nginx

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ do
     echo "Waiting for db to be ready..."
     sleep 2
 done
-
-python manage.py runserver 0.0.0.0:8080
+python manage.py collectstatic
+gunicorn --bind 0.0.0.0:8080 notifications.wsgi
