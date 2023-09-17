70e1c0450819a824d00a7d9a36309b5cf1cce636
MehMasha <mehmasha69@gmail.com>
2023-09-18 00:44:24 +0300

    celery

--- notifications/settings.py
+++ notifications/settings.py
@@ -87,3 +87,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
